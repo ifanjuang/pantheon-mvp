0686d2692edb10381288437feb26c2844389cbbd
ifanjuang <3180373+ifanjuang@users.noreply.github.com>
2026-07-29 18:47:12 +0200

    fix(cockpit): retain V3 shell controls in collection demo

--- mvp_vertical/cockpit/v3_bootstrap.js
+++ mvp_vertical/cockpit/v3_bootstrap.js
@@ -2,5 +2,9 @@ document.documentElement.dataset.cockpitVersion = "3";
 document.documentElement.classList.add("cockpit-v3");
 
 const params = new URLSearchParams(window.location.search);
-if (params.get("mode") === "demo") await import("./v3/demo_collection_app.js");
-else await import("./v2_bootstrap.js");
+if (params.get("mode") === "demo") {
+  await import("./v3/demo_collection_app.js");
+  await import("./v2_shell_controls.js");
+} else {
+  await import("./v2_bootstrap.js");
+}
